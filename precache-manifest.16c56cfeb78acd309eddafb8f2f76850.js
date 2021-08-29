7b018b1aed63bda916b071c4a7b1574a01aa5923
Inhoi Heo <inhoi.heo@oecd.org>
2021-08-30 00:56:18 +0200

    Updates

---
+++ precache-manifest.16c56cfeb78acd309eddafb8f2f76850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed59fbfbc13412b08b4dcaf8c5a19987",
+    "url": "/greentech/index.html"
+  },
+  {
+    "revision": "acaf4bd5bd12b79bd9fe",
+    "url": "/greentech/static/css/main.803d1a74.chunk.css"
+  },
+  {
+    "revision": "6808692ad8759b4ab52c",
+    "url": "/greentech/static/js/2.355347e3.chunk.js"
+  },
+  {
+    "revision": "1fbd3b3b57187c830d43cf1b0fb1d8cb",
+    "url": "/greentech/static/js/2.355347e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acaf4bd5bd12b79bd9fe",
+    "url": "/greentech/static/js/main.c279a679.chunk.js"
+  },
+  {
+    "revision": "4744951bbd17d2932937",
+    "url": "/greentech/static/js/runtime-main.6c19957b.js"
+  }
+]);
